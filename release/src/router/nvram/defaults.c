d8a68b84441b0ff5b5757cce4ec452e876e720fc
Shibby <shibby@openlinksys.info>
2014-01-15 23:00:32 +0100

    TomatoAnon: lot of changes
    * update databate only once per week
    * check new tomato version once per day (only for shibby mod)
    * fix "undefinded" tomato version on Status page

--- release/src/router/nvram/defaults.c
+++ release/src/router/nvram/defaults.c
@@ -691,7 +691,6 @@ const defaults_t defaults[] = {
 // admin-tomatoanon
 	{ "tomatoanon_enable",		"-1"				},
 	{ "tomatoanon_answer",		"0"				},
-	{ "tomatoanon_cru",		"6"				},
 	{ "tomatoanon_id",		""				},
 	{ "tomatoanon_notify",		"1"				},
 
